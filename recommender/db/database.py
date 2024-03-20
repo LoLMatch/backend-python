7a8203b83636cbf387464471b2e52db50bddc075
KubaKrzych <krzychowiec.k@gmail.com>
2024-03-20 09:46:36 +0100

    Fix compose and docker

--- recommender/db/database.py
+++ recommender/db/database.py
@@ -2,14 +2,16 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 
+
 def get_db_connection():
     return psycopg2.connect(
-        dbname="lolmatch",
-        user="postgres",
-        password="admin",
-        port="5432",
-        host="localhost"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        host=os.getenv("DB_HOST"),
     )
+
 
 def fetch_one(query, params=None):
     with get_db_connection() as conn:
@@ -17,11 +19,13 @@ def fetch_one(query, params=None):
             cur.execute(query, params)
             return cur.fetchone()
 
+
 def fetch_all(query, params=None):
     with get_db_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, params)
             return cur.fetchall()
+
 
 def execute_query(query, conn=get_db_connection, params=None, commit=False):
     with conn.cursor() as cur:
